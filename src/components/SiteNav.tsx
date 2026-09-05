67e3c01b5adee4d1a3689a34c0d87d73a4fed94c
Denis Simon <mail.simon.denis@gmail.com>
2026-09-05 15:34:06 +0200

    Use Imperial Script font for 'enis Simon' header name

--- src/components/SiteNav.tsx
+++ src/components/SiteNav.tsx
@@ -47,7 +47,7 @@ export function SiteNav() {
         {/* Logo and Name */}
         <Link to="/#works" className="h-8 md:h-10 flex items-end">
           <img src={logo} alt="D" className="h-full object-contain" />
-          <span className="font-serif font-bold leading-none text-[rgb(28,66,111)] text-2xl md:text-3xl tracking-wide ml-0.5">
+          <span className="font-script leading-none text-[rgb(28,66,111)] text-2xl md:text-3xl ml-0.5">
             enis Simon
           </span>
         </Link>
